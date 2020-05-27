c87c51724a053ce9045cd741b87c57243f39a670
lisivka <lisivka@gmail.com>
2020-05-27 20:48:34 +0300

    Шаблонизация страниц

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
 from .views import *
-from .views import index, about, contact
+from .views import index, about, contact, index2, blog, blog_details, blog_right, blog_left
+from .views import service, service_details, shop
 
 
 urlpatterns = [
     path('', index),
     path('index', index),
+    path('index2', index2),
+    path('index3', index3),
     path('about', about),
+    path('service', service),
+    path('service-details', service_details),
+
+    path('blog', blog),
+    path('blog-left', blog_left),
+    path('blog-right', blog_right),
+    path('blog-details', blog_details),
+    path('shop', shop),
     path('contact', contact)
 
 
--- home/views.py
+++ home/views.py
@@ -4,9 +4,35 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'home/index.html')
 
+def index2(request):
+    return render(request, 'home/index2.html')
+
+def index3(request):
+    return render(request, 'home/index3.html')
 
 def about(request):
     return render(request, 'home/about.html')
+
+def blog(request):
+    return render(request, 'home/blog.html')
+
+def blog_left(request):
+    return render(request, 'home/blog-left.html')
+
+def blog_right(request):
+    return render(request, 'home/blog-right.html')
+
+def blog_details(request):
+    return render(request, 'home/blog-details.html')
+
+def service(request):
+    return render(request, 'home/service.html')
+
+def service_details(request):
+    return render(request, 'home/service-details.html')
+
+def shop(request):
+    return render(request, 'home/shop.html')
 
 
 def contact(request):
