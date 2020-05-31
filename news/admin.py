c0cea7caf649d5cf067049ee9a20bc4804d58561
lisivka <lisivka@gmail.com>
2020-05-31 09:26:02 +0300

    Add Sign in and Admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Forex, Stock, Commodity, Crypto
+admin.site.register(Category)
+admin.site.register(Forex)
+admin.site.register(Stock)
+admin.site.register(Commodity)
+admin.site.register(Crypto)
+
+
+
